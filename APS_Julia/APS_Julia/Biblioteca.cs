ef1c711bdd0ecf253e470e1212f9a71b382836c0
JULIA LOUBACK RIBEIRO <2019102092@unibrasil.com.br>
2019-09-11 19:26:02 -0300

    Alteracao

---
+++ APS_Julia/APS_Julia/Biblioteca.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APS_Julia
+{
+    class Biblioteca
+    {
+        public string Id { get; set; }
+        public string Nome { get; set; }
+        public string Autor { get; set; }
+        public string Editora { get; set; }
+    }
+}
